14fb40e12a6f2625c8b1910afdf3c6188770282f
Resham <reshamupadhyay48@gmail.com>
2026-05-11 22:34:18 +0530

    added reap for cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -12,9 +12,11 @@ type cacheEntry struct {
 }
 
 func NewCache(interval time.Duration) Cache {
-	return Cache{
+	c := Cache{
 		cache: make(map[string]cacheEntry),
 	}
+	go c.reapLoop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
@@ -29,6 +31,18 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	return cacheE.val, ok
 }
 
-func (c *Cache) reapLoop() {
-	
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C{
+		c.reap(interval)
+	}
+}
+
+func (c *Cache) reap(interval time.Duration) {
+	timeAgo := time.Now().UTC().Add(-interval)
+	for k, v := range c.cache{
+		if v.createdAt.Before(timeAgo) {
+			delete(c.cache,k)
+		}
+	}
 }
--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -14,17 +14,52 @@ func TestCreateCache(t *testing.T) {
 }
 
 func TestAddGetCache(t *testing.T) {
-	cache := NewCache(time.Millisecond)
-	cache.Add("key1", []byte("val1"))
-
-	actual, ok := cache.Get("key1")
-	if !ok {
-		t.Error("key1 not found")
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "",
+			inputVal: []byte("val3"),
+		},
 	}
 
-	if string(actual) != "val1" {
-		t.Error("value doesn't match")
+	for _, cas := range cases {
+		cache := NewCache(time.Millisecond)
+		cache.Add(cas.inputKey, cas.inputVal)
+
+		actual, ok := cache.Get(cas.inputKey)
+		if !ok {
+			t.Errorf("%v not found", cas.inputKey)
+			continue
+		}
+
+		if string(actual) != string(cas.inputVal) {
+			t.Errorf("%v doesn't match %s", string(cas.inputVal), string(actual))
+			continue
+		}
 	}
 
 }
 
+func TestReap(t *testing.T) {
+	interval := time.Millisecond * 10
+	cache := NewCache(interval)
+	inputKey := "key1"
+	inputVal := []byte("val1")
+	cache.Add(inputKey, inputVal)
+	time.Sleep(interval + time.Microsecond)
+
+	_, ok := cache.Get(inputKey)
+	if ok {
+		t.Errorf("%s should have been reaped",inputKey)
+	} 
+}
